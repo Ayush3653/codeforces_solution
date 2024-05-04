b6e42a8e5c4e55957ae832ab9405520054bfda4b
Ayush3653 <156705859+Ayush3653@users.noreply.github.com>
2024-05-04 13:46:15 +0530

    Add files via upload

---
+++ 664A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(NULL);
+    string a,b;
+    cin>>a>>b;
+    if(a==b){
+        cout<<a;
+    }
+    else{
+        cout<<"1";
+    }
+    return 0;
+}
